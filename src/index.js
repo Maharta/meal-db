3b19cd6b626601332ca1f09e3b270147ad3fa5a0
Maharta <gedemaharta.dev@gmail.com>
2022-03-26 07:06:01 +0800

    added dependencies 'regenerator-runtime'

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import 'regenerator-runtime';
 import "./styles/style.css";
 import "./script/components/app-bar";
 import "./script/components/searchbar";
 import main from "./script/view/main";
 
-//  image for header
 
 
 main();
